1b623eccbc1cfceff285e20afc36e5c37dc9f387
Vanessa Vieira <105254938+VanessaVieira34@users.noreply.github.com>
2022-08-16 17:50:39 -0300

    Add files via upload

---
+++ aula7.js
@@ -0,0 +1,24 @@
+/*
+Prática aula 7
+
+Exercício 1 (part 1)
+
+1 false
+2 true
+3 true
+4 true
+5 false
+6 true
+7 true
+8 false
+9 false
+10 a. false
+10 b. true
+10 c. true
+
+Exercício 2 (part 2)
+
+1 true
+2 false
+3 false
+/*
